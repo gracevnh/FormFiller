13ebd0c8171951140cea38abde413999e69c5584
kennethou <oukennet@usc.edu>
2025-03-08 12:57:11 -0800

    profile class + getProfile + testButton(to be removed)

--- sidepanel.js
+++ sidepanel.js
@@ -1,3 +1,28 @@
+class Profile {
+    constructor(data) {
+        this.data = data
+    }
+    get fname() {
+        return this.data.fname;
+    }
+    get lname() {
+        return this.data.lname;
+    }
+    get fullname() {
+        return this.data.fname + " " + this.data.lname;
+    }
+    get dob() {
+        return this.data.dob;
+    }
+    get phone() {
+        return this.data.phone;
+    }
+    get email() {
+        return this.data.email;
+    }
+}
+
+// update this when new fields are added
 const fname = document.getElementById("fname");
 const lname = document.getElementById("lname");
 const email = document.getElementById("email");
@@ -43,8 +68,45 @@ saveButton.addEventListener("click", function () {
   data.phone = phone.value;
   data.dob = dob.value;
 
+  // need to change "input" to something else. currently it is just a dummy name
   chrome.storage.local.set({ input: data }, function () {
     console.log("Data saved!", data);
     alert("Form submitted successfully!");
   });
+});
+
+function getProfile(name) {
+    // Promise because chrome.storage.local.get is async
+    return new Promise(function(resolve, reject) {
+        chrome.storage.local.get(name, function(result) {
+            if (result[name]) {
+                const userProfile = new Profile(result[name]);
+                resolve(userProfile);
+            } else {
+                reject("Profile not found.");
+            }
+        });
+    });
+}
+
+// remove this when done, test code
+const testButton = document.getElementById("test")
+testButton.addEventListener("click", function () {
+    console.log("Button clicked");
+    // test
+    getProfile("input")
+    .then(function(userProfile) {
+      document.getElementById("fname").value = userProfile.fname || "";
+      document.getElementById("lname").value = userProfile.lname || "";
+      document.getElementById("email").value = userProfile.email || "";
+      document.getElementById("phone").value = userProfile.phone || "";
+      document.getElementById("dob").value = userProfile.dob || "";
+
+      console.log("Profile loaded successfully:", userProfile);
+      alert("Profile loaded successfully!");
+    })
+    .catch(function(error) {
+      console.log(error);
+      alert(error);
+    });
 });
